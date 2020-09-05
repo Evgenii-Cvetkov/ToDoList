c42fd1d8086ccfd60099e2cea96ac7cbc16715f1
Evgenii <johny360@mail.ru>
2020-09-05 15:51:44 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+var input = document.querySelector(".block__form-input");
+    btn = document.querySelector(".block__form-btn");
+    blockList = document.querySelector(".block__list");
+   
+function createElements(text) {
+    var newElem = document.createElement("p"); 
+    var deleteSpan = document.createElement("span");
+    
+    deleteSpan.className = "block__list-delete";
+    deleteSpan.textContent = "delete";
+    
+
+    newElem.className = "block__list-elem";
+    newElem.textContent = text;
+    newElem.appendChild(deleteSpan);
+
+    deleteSpan.addEventListener("click", function(){
+        blockList.removeChild(newElem);
+    });
+
+    blockList.appendChild(newElem); 
+}  
+
+
+
+
+
+btn.addEventListener("click", function(e){
+    e.preventDefault();
+    if(!(input.value === "enter: " || input.value === "")) {
+        createElements(input.value);
+        input.value = "enter: ";
+    }
+
+});
+
+input.addEventListener("focus", function(){
+    input.value = "";
+});
+
+input.addEventListener("blur", function(){
+    if(input.value === "")
+        input.value = "enter: ";
+});
